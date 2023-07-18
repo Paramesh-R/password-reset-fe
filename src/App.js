291777b9b3c168d34fa22d7b1541008415d17697
Paramesh-R <parameswaran.r@outlook.com>
2023-07-18 17:19:14 +0530

    removed MainRoute

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import MainRoutes from './MainRoutes';
+import { Route, Routes } from 'react-router-dom'
+import SignIn from './pages/Auth/SignIn'
+import PrivateRoutes from './components/RouteRestriction/PrivateRoutes'
+import Dashboard from './pages/Dashboard/Dashboard'
+import Error404 from './pages/Home/Error404'
+import SignUp from './pages/Auth/SignUp'
+import ForgotPassword from './pages/Auth/ForgotPassword'
+import ThanksPage from './pages/Auth/ThanksPage'
+import ChangePassword from './pages/Auth/ChangePassword'
+import ActivateAccount from './pages/Auth/ActivateAccount'
 
 function App() {
   return (
     <div className="App">
-      <MainRoutes />
+      <Routes>
+        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/signUp" element={<SignUp />} />
+
+        <Route path="/forgotPassword" element={<ForgotPassword />} />
+        <Route path="/success" element={<ThanksPage />} />
+
+        <Route path="/activate-account/:activationToken" element={<ActivateAccount />} />
+        <Route path="/resetpassword/:resetToken" element={<ChangePassword />} />
+        {/* Protected Route */}
+        <Route path="/" element={<PrivateRoutes />}>
+          <Route path="/" element={<Dashboard />} />
+        </Route>
+
+        {/* Error 404 */}
+        <Route path="*" element={<Error404 />} />
+      </Routes >
     </div>
   );
 }
